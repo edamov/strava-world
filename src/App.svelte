a2a8d02bb83776f0542738072c5af38c9edb9fd7
Arthur Edamov <edamov@gmail.com>
2021-12-18 11:39:39 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,10 @@
 	export let name;
 
 	let result = '';
-	let text = '';
+	let info = '';
+	let token = null;
+	let activities = [];
+	let total = 0;
 
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
@@ -17,8 +20,34 @@
 
 		xhr.onload = function() {//Вызывает функцию при смене состояния.
 			const obj = JSON.parse(xhr.response);
-			name = obj.athlete.firstname;
-			result = obj.athlete.firstname + ' ' + obj.athlete.lastname
+			if (!obj.hasOwnProperty('athlete')) {
+				return;
+			}
+			info = obj.athlete.firstname + ' ' + obj.athlete.lastname
+			token = obj.access_token;
+
+			var xhr2 = new XMLHttpRequest();
+			xhr2.open("GET", 'https://www.strava.com/api/v3/athlete/activities?per_page=100&' + 'access_token=' + token , true);
+
+			xhr2.onload = function () {
+				const result = JSON.parse(xhr2.response);
+
+				result.forEach(element => {
+					activities.push({
+						distance: element.distance,
+						name: element.name,
+						type: element.type
+					});
+
+					total += element.distance;
+				});
+
+				activities = activities;
+
+				console.log(activities);
+			}
+
+			xhr2.send();
 		}
 		xhr.send(`client_id=75539&client_secret=333336ab8d983b96baef6a6c0c36bfe7b0ef374b&code=${code}&grant_type=authorization_code`);
 	// } else {
@@ -26,7 +55,7 @@
 	}
 
 	function handleClick() {
-		window.location = `http://www.strava.com/oauth/authorize?client_id=75539&response_type=code&redirect_uri=https://strava-world.vercel.app&approval_prompt=force&scope=read`;
+		window.location = `http://www.strava.com/oauth/authorize?client_id=75539&response_type=code&redirect_uri=https://strava-world.vercel.app&approval_prompt=force&scope=activity:read_all`;
 	}
 
 
@@ -40,8 +69,19 @@
 	</button>
 
 	<pre>
-{result}
+{info}
 </pre>
+	<div>
+		Сумарна дистанція за останні 100 активностей: <b>{Math.round(total)} метрів</b>
+		<br>
+		<br>
+		<br>
+		{#each activities as activity}
+				Назва: {activity.name}<br>
+				Тип: {activity.type}<br>
+				Дистанція: {activity.distance}<br><br>
+		{/each}
+	</div>
 </main>
 
 <style>
