736ffa12104b4df1f59ac1eeee030b17809260f1
Arthur Edamov <edamov@gmail.com>
2021-12-18 09:25:34 +0200

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,70 @@
 <script>
 	export let name;
 
-	// var strava = require('strava-v3')
-    // strava.config({
-    //   "access_token"  : "Your apps access token (Required for Quickstart)",
-    //   "client_id"     : "Your apps Client ID (Required for oauth)",
-    //   "client_secret" : "Your apps Client Secret (Required for oauth)",
-    //   "redirect_uri"  : "Your apps Authorization Redirection URI (Required for oauth)",
-    // });
+	let result = null;
+
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (urlParams.has('code')) {
+		const code = urlParams.get('code');
+		console.log(code);
+
+		doPost();
+
+	// 	curl -X POST https://www.strava.com/api/v3/oauth/token \
+	// 			-d client_id=75539 \
+  // -d client_secret=333336ab8d983b96baef6a6c0c36bfe7b0ef374b \
+  // -d code=352f251097adc5f09513e56526b761e935051788 \
+  // -d grant_type=authorization_code
+
+		async function doPost () {
+			const res = await fetch('https://www.strava.com/api/v3/oauth/token', {
+				method: 'POST',
+				body: JSON.stringify({
+					client_id: 75539,
+					client_secret: "333336ab8d983b96baef6a6c0c36bfe7b0ef374b",
+					code: code,
+					grant_type: "authorization_code"
+				})
+			})
+
+			const json = await res.json()
+			result = JSON.stringify(json)
+		}
+
+		// var xhr = new XMLHttpRequest();
+		// xhr.open("POST", 'https://www.strava.com/api/v3/oauth/token', true);
+	//
+	// 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	//
+	// 	xhr.onreadystatechange = function() {//Вызывает функцию при смене состояния.
+	// 		if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+	//
+	// 		}
+	// 		console.log(xhr.statusText)
+	// 	}
+	// 	xhr.send(`client_id=75539&client_secret=333336ab8d983b96baef6a6c0c36bfe7b0ef374b&code=${code}&grant_type=authorization_code`);
+	// } else {
+	//
+	}
+
+	function handleClick() {
+		window.location = `http://www.strava.com/oauth/authorize?client_id=75539&response_type=code&redirect_uri=http://localhost:5000&approval_prompt=force&scope=activity:read_all`;
+	}
+
+
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+
+	<button on:click={handleClick}>
+		Connect to Strava
+	</button>
+
+	<pre>
+{result}
+</pre>
 </main>
 
 <style>
